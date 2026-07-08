c9c5aa8b40aca37906c0a946f9c092b5d3c3e184
m2构造机 <zhanggaoqi@aobi.com>
2026-07-08 15:43:47 +0800

    [Branch Release] v2.0.2-dev-1481108 - develop
    - 更新所有 XCFrameworks 到 2.0.2-dev-1481108
    - 更新资源文件
    - 更新 checksums
    - 分支测试版本（无 tag）

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
         // ========== Binary Frameworks ==========
         .binaryTarget(
             name: "IPv6Support",
-            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/IPv6Support/1.0.4-dev-1474900/IPv6Support.xcframework.zip",
-            checksum: "7d9d62dea8d696ece58d0f19fd6b4654a4ca1d774d0c77f4f33e10b259e32573"
+            url: "https://yw-depot-nexus.100bt.com/repository/onesdk-ios-trunk/spm/IPv6Support/2.0.2-dev-1481108/IPv6Support.xcframework.zip",
+            checksum: "34e3b7d6ca75dc23cfae80b7185869669eaa3bb7298b32a8f8d97d6c88f785df"
         )
     ]
 )
